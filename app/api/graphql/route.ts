a660d564793e6837a80195a9251b613b241497e0
mohit-p41 <mohit.hiralal@particle41.com>
2023-11-02 20:53:15 +0530

    Temporarily disable graphql route

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,21 +1,27 @@
-import { ApolloServer } from "@apollo/server";
+// import { ApolloServer } from "@apollo/server";
 import db from "../../../prisma/db";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
+// import { startServerAndCreateNextHandler } from "@as-integrations/next";
+
+import { NextRequest } from "next/server";
 
 import { PrismaClient } from "@prisma/client";
-import { NextApiRequest } from "next";
-import { typeDefs } from "@/graphql/schema";
-import { resolvers } from "@/graphql/resolvers";
+// import { NextApiRequest } from "next";
+// import { typeDefs } from "@/graphql/schema";
+// import { resolvers } from "@/graphql/resolvers";
 
 export type Context = {
     prisma?: PrismaClient
 };
 
-const server: ApolloServer = new ApolloServer<Context>({
-    resolvers,
-    typeDefs
-})
+// const server: ApolloServer = new ApolloServer<Context>({
+//     resolvers,
+//     typeDefs
+// })
 
-export default startServerAndCreateNextHandler(server, {
-    context: async (req, res) => ({ req, res, db }),
-});
+// export default startServerAndCreateNextHandler(server, {
+//     context: async (req, res) => ({ req, res, db }),
+// });
+
+export const DELETE = async (request: NextRequest): Promise<any> => {
+
+}
